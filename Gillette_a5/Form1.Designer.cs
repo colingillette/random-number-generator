a662e685b94032f2a7cd945a9f35b29f524db477
MSUStudent <MSUStudent@DESKTOP-4UQFETR>
2018-04-03 10:50:24 -0500

    Add project files.

---
+++ Gillette_a5/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Gillette_a5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbersLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numbersLabel
+            // 
+            this.numbersLabel.AutoSize = true;
+            this.numbersLabel.Location = new System.Drawing.Point(94, 169);
+            this.numbersLabel.Name = "numbersLabel";
+            this.numbersLabel.Size = new System.Drawing.Size(177, 13);
+            this.numbersLabel.TabIndex = 0;
+            this.numbersLabel.Text = "Number of Random Numbers (0-99):";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(292, 166);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(122, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(137, 302);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save File*";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(313, 302);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 3;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "* Be sure to include \".txt\" at the end of the file name.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 435);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.numbersLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numbersLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Gillette_a5/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Gillette_a5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile;
+            Random ran = new Random();
+
+
+            int.TryParse(numberTextBox.Text, out int iterations);
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                outputFile = File.CreateText(saveFileDialog1.FileName);
+                for (int i = 0; i < iterations; i++)
+                {
+                    outputFile.WriteLine(ran.Next(0, 100).ToString());
+                }
+
+                MessageBox.Show("File Saved");
+                outputFile.Close();
+            }
+            else
+            {
+                MessageBox.Show("Save canceled.");
+            }
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+            int reader;
+            int zeroToNineteen = 0, twentyToThirtyNine = 0, fourtyToFiftyNine = 0,
+                sixtyToSeventyNine = 0, eightyToNinetyNine = 0;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = File.OpenText(openFileDialog1.FileName);
+
+                try
+                {
+                    while (!inputFile.EndOfStream)
+                    {
+                        reader = int.Parse(inputFile.ReadLine());
+                        if (reader < 20)
+                        {
+                            zeroToNineteen += 1;
+                        }
+                        else if (reader < 40)
+                        {
+                            twentyToThirtyNine += 1;
+                        }
+                        else if (reader < 60)
+                        {
+                            fourtyToFiftyNine += 1;
+                        }
+                        else if (reader < 80)
+                        {
+                            sixtyToSeventyNine += 1;
+                        }
+                        else
+                        {
+                            eightyToNinetyNine += 1;
+                        }
+                    }
+
+                    MessageBox.Show($"{zeroToNineteen}, {twentyToThirtyNine}, {fourtyToFiftyNine}, " +
+                        $"{sixtyToSeventyNine}, {eightyToNinetyNine}");
+                    inputFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No file selected.");
+            }
+        }
+    }
+}
